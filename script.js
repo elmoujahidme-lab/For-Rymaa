ba2f4b823f72560f9c291086a43fce9a2891d9c0
elmoujahidme-lab <elmoujahidme@gmail.com>
2026-08-01 08:29:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -91,6 +91,8 @@ const hours=Math.floor((diff/(1000*60*60))%24);
 
 const minutes=Math.floor((diff/(1000*60))%60);
 
+const seconds=Math.floor((diff/1000)%60);
+
 document.getElementById("days").textContent=days;
 
 document.getElementById("hours").textContent=hours;
